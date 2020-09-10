738cb380f2e3338de06a3812068488fc7c293f61
schmudde <d@schmud.de>
2020-09-10 17:55:53 +0200

    final filter and output

--- src/core.clj
+++ src/core.clj
@@ -1,6 +1,7 @@
 (ns core
   (:require [clojure.pprint :refer [pprint pp]]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.edn :as edn]))
 
 (def tweets (json/read-str (slurp "src/tweets-full.js") :key-fn keyword))
 
@@ -11,4 +12,22 @@
    (sort-by #(get-in % [:tweet :favorite_count]))
    (spit "target/tweets-filtered.edn")))
 
-#_(filter #(when (= (get-in % [:tweet :entities :hashtags 0 :text]) "侘寂") %) tweets)
+(defn get-stats [stat coll]
+  (read-string (get-in coll stat)))
+
+(defn get-top-aphorisms []
+  (let [tweets (edn/read-string (slurp "target/tweets-filtered.edn"))
+        favorites (partial get-stats [:tweet :favorite_count])
+        retweets (partial get-stats [:tweet :retweet_count])]
+    (->>
+     (filter #(when (> (+ (favorites %) (retweets %)) 2) %) tweets)
+     (map #(get-in % [:tweet :full_text]))
+     (reverse)
+     (pprint))))
+
+
+(comment
+
+  (count (edn/read-string (slurp "target/tweets-filtered.edn"))) ;; 210
+
+  )
