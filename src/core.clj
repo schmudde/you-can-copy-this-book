746a341aaab08eeab55e2ef0b0ef0c70011b71db
schmudde <d@schmud.de>
2020-09-10 17:02:05 +0200

    cleanup source and destination tweet data

--- src/core.clj
+++ src/core.clj
@@ -2,12 +2,13 @@
   (:require [clojure.pprint :refer [pprint pp]]
             [clojure.data.json :as json]))
 
-(def tweets (json/read-str (slurp "src/tweets.js") :key-fn keyword))
+(def tweets (json/read-str (slurp "src/tweets-full.js") :key-fn keyword))
 
 (defn -main [& args]
   (println "Begin Tweet Scan")
   (->>
    (filter #(when (= (get-in % [:tweet :entities :hashtags 0 :text]) "侘寂") %) tweets)
-   (sort-by #(get-in % [:tweet :favorite_count]))))
+   (sort-by #(get-in % [:tweet :favorite_count]))
+   (spit "target/tweets-filtered.edn")))
 
 #_(filter #(when (= (get-in % [:tweet :entities :hashtags 0 :text]) "侘寂") %) tweets)
